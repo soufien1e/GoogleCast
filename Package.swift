cc6bcd7d6a6344b2a536f1c5159ee678919e4c99
SOFIENE BOUHLEL <sbouhlel.olympp@tf1.fr>
2021-12-29 18:19:48 +0100

    update GoogleCast package

--- Package.swift
+++ Package.swift
@@ -10,22 +10,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GoogleCast",
-            targets: ["GoogleCast", "GoogleCastLib"]),
+            targets: ["GoogleCast"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GoogleCast",
-            dependencies: []),
         .binaryTarget(
-            name: "GoogleCastLib",
-            path: "Sources/GoogleCast/GoogleCast.xcframework"
+            name: "GoogleCast",
+            path: "GoogleCast.xcframework"
         )
-        
     ]
 )
